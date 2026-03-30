c54c1852adbe4c03b188481e57e9d62d33430048
Raffaele Khalijy <r.khamlijy@studenti.unimarconi.it>
2026-03-30 18:57:17 +0200

    Setup project folders and empty files

---
+++ mental_math_trainer/trayner.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 18:45:59 2026
+
+@author: Medion
+"""
+
---
+++ tests/tests_trainer.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 18:46:32 2026
+
+@author: Medion
+"""
+
